25e10d23cc9ad0a7f56a574a79c873cacdd9dcab
kumankoishi.py <7iggif@naver.com>
2021-04-22 21:47:22 +0900

    Make it resized to fit canvas

--- js/app.js
+++ js/app.js
@@ -65,8 +65,12 @@ class App {
         if (event.target.readyState === FileReader.DONE) {
           let img = new Image();
           img.src = event.target.result;
+
+          //const targetHeight = canvas.height / img.height;
+          //const targetWidth = canvas.width / img.width;
+
           img.onload = () => {
-            this.ctx.drawImage(img, 0, 0);
+            this.ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, this.canvas.width, this.canvas.height);
           }
         }
       }
